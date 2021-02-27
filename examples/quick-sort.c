9e2c1c41bd12cda63c126c2e2bd3d3471bf1b952
RinHizakura <s921975628@gmail.com>
2021-02-27 22:46:53 +0800

    Implement quick sort with non-recursive version

--- examples/quick-sort.c
+++ examples/quick-sort.c
@@ -1,8 +1,8 @@
 #include <assert.h>
 #include <stdlib.h>
-#include "list.h"
 
 #include "common.h"
+#include "list.h"
 
 static uint16_t values[256];
 
@@ -36,6 +36,52 @@ static void list_qsort(struct list_head *head)
     list_splice_tail(&list_greater, head);
 }
 
+#define MAX_LEN 1000
+static void list_qsort_no_recursive(struct list_head *head)
+{
+    struct listitem *begin[MAX_LEN], *end[MAX_LEN], *L, *R;
+    struct listitem pivot;
+    int i = 0;
+
+    begin[0] = list_first_entry(head, struct listitem, list);
+    end[0] = list_last_entry(head, struct listitem, list);
+
+    while (i >= 0) {
+        L = begin[i];
+        R = end[i];
+
+        if (L != R && &begin[i]->list != head) {
+            // pivot is the actual address of L
+            pivot = *begin[i];
+            if (i == MAX_LEN - 1) {
+                assert(-1);
+                return;
+            }
+
+            while (L != R) {
+                while (R->i >= pivot.i && L != R)
+                    R = list_entry(R->list.prev, struct listitem, list);
+                if (L != R) {
+                    L->i = R->i;
+                    L = list_entry(L->list.next, struct listitem, list);
+                }
+
+                while (L->i <= pivot.i && L != R)
+                    L = list_entry(L->list.next, struct listitem, list);
+                if (L != R) {
+                    R->i = L->i;
+                    R = list_entry(R->list.prev, struct listitem, list);
+                }
+            }
+            L->i = pivot.i;
+            begin[i + 1] = list_entry(L->list.next, struct listitem, list);
+            end[i + 1] = end[i];
+            end[i++] = L;
+        } else
+            i--;
+    }
+}
+
 int main(void)
 {
     struct list_head testlist;
@@ -58,7 +104,7 @@ int main(void)
     assert(!list_empty(&testlist));
 
     qsort(values, ARRAY_SIZE(values), sizeof(values[0]), cmpint);
-    list_qsort(&testlist);
+    list_qsort_no_recursive(&testlist);
 
     i = 0;
     list_for_each_entry_safe (item, is, &testlist, list) {
